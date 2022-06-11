076a2067d63b361c367792c70c9789fb0225c92e
Rashed <rashedamlan02@gmail.com>
2022-06-11 21:23:12 +0600

    initial setUp and create done

---
+++ Payment-App-Project/Angular_DotNet_5_CRUD/Angular_DotNet_5_CRUD/DataContext/PaymentDetailContext.cs
@@ -0,0 +1,19 @@
+﻿using Angular_DotNet_5_CRUD.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Angular_DotNet_5_CRUD.DataContext
+{
+    public class PaymentDetailContext:DbContext
+    {
+        public PaymentDetailContext(DbContextOptions<PaymentDetailContext> options):base(options) 
+        {
+        
+        
+        }
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
+    }
+}
---
+++ Payment-App-Project/Angular_DotNet_5_CRUD/Angular_DotNet_5_CRUD/Models/PaymentDetail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Angular_DotNet_5_CRUD.Models
+{
+    public class PaymentDetail
+    {
+        [Key]
+        public int PaymentDetaiId { get; set; }
+        public string CardOwnerName { get; set; }
+        public string CardNumber { get; set; }
+        public string ExpirationDate { get; set; }
+        public string SecurityCode { get; set; }
+
+    }
+}
